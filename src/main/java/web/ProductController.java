d4159413e93ba84967b1c58d9bac2000587117bc
Yaroslav Hulaga <gulaga.yaroslav@gmail.com>
2015-12-02 21:40:13 +0200

    Spring test

--- src/main/java/web/ProductController.java
+++ src/main/java/web/ProductController.java
@@ -1,26 +1,28 @@
 package web;
 
-import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import dto.Product;
-import service.ProductService;
-
 @RequestMapping("/api")
 public class ProductController {
 
-    private ProductService productService;
+	// private ProductService productService;
+	//
+	// public ProductController(ProductService productService) {
+	// this.productService = productService;
+	// }
+	//
+	// @RequestMapping(value = "/products", method = RequestMethod.GET)
+	// public ResponseEntity<List<Product>> getProducts() {
+	// return new
+	// ResponseEntity<List<Product>>(productService.getSomeProducts(),
+	// HttpStatus.OK);
+	// }
 
-    public ProductController(ProductService productService) {
-	this.productService = productService;
-    }
-
-    @RequestMapping(value = "/products", method = RequestMethod.GET)
-    public ResponseEntity<List<Product>> getProducts() {
-	return new ResponseEntity<List<Product>>(productService.getSomeProducts(), HttpStatus.OK);
-    }
+	@RequestMapping(value = "/products", method = RequestMethod.GET)
+	public ResponseEntity<String> getProducts() {
+		return new ResponseEntity<String>("some list", HttpStatus.OK);
+	}
 }
